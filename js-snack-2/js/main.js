8dd706379b896d9b478a646b47a97e3840a3b969
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-02-28 10:59:52 +0100

    added trim

--- js-snack-2/js/main.js
+++ js-snack-2/js/main.js
@@ -1,8 +1,8 @@
 const result = document.getElementById('result');
 
 document.getElementById('submit').addEventListener('click', () => {
-  const parola1 = document.getElementById('input-uno').value;
-  const parola2 = document.getElementById('input-due').value;
+  const parola1 = document.getElementById('input-uno').value.trim();
+  const parola2 = document.getElementById('input-due').value.trim();
 
   if (parola1.length > parola2.length) {
     result.innerHTML = `È più lunga la parola ${parola1}`;
